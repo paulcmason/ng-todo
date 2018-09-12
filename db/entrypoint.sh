92f5016a2802b25b109c1e6d35388b79bcc74acd
Paul Mason <paul.mason@apteco.com>
2018-09-12 15:23:26 +0100

    Add in sql server db running in linux container

---
+++ db/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Debug"
+
+# Start SQL Server
+/opt/mssql/bin/sqlservr &
+
+# Start the script to create the DB and user
+/usr/config/configure-db.sh
+
+# Call extra command
+eval $1
